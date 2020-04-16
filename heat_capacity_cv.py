eb3ae665a121d0894465882f7bb62d10133a5dc1
paullafaye <62479777+paullafaye@users.noreply.github.com>
2020-04-16 16:13:15 -0400

    Update heat_capacity_cv.py

--- heat_capacity_cv.py
+++ heat_capacity_cv.py
@@ -1,16 +1,19 @@
-from scipy.integrate import quad
-from math import *
+from sympy import *
+from math import exp, floor
 import numpy as np
 
 
+x = symbols('x')
 na = 6.022140857*10**23
 kb = 0.00008617330336
 
 def cv_vib(thetaD, tm):    
     global na
     global kb
-    for t in np.linspace(0, floor(tm), 100):
-        return t
+    for t in np.linspace(1, tm, 100):
+        print(9*na*kb*((t/thetaD)**3))*integrate(x**4*exp(x)/(exp(x)-1)**2, (x, 0.01, thetaD/t))
 
-# 9*na*kb*((t/thetaD)**3)*quad((x**4*np.exp(x))/((np.exp(x)-1)**2), 0, thetaD/t)
-print(cv_vib(376, 933))
+
+if __name__ == "__main__": 
+    print(cv_vib(376, 933))
+
