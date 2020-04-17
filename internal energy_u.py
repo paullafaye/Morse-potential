8bea646ffb0f544bf2b71a55e00d76a8bfb806e3
paullafaye <62479777+paullafaye@users.noreply.github.com>
2020-04-17 13:12:05 -0400

    Create internal energy_u.py

---
+++ internal energy_u.py
@@ -0,0 +1,19 @@
+from scipy.integrate import quad
+import math
+import numpy as np
+
+na = 6.022140857*10**23
+kb = 1.380649*10**(-23)
+
+def debye(x):
+    return (x**3)/(((np.exp(x)-1)))
+
+def u(t, thetaD):
+    return 9*na*kb*((t/thetaD)**3)*quad(debye, 0, thetaD/t)[0]
+
+vec_u = np.vectorize(u)
+
+def u_vib(thetaD, tm):
+    return(vec_u(np.arange(1.0, tm, 10), thetaD))
+
+print(u_vib(376, 933))
