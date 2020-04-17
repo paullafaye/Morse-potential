53fa26e3a192c3ddb6e325259d06a19480414ed1
paullafaye <62479777+paullafaye@users.noreply.github.com>
2020-04-17 17:29:52 -0400

    Update internal energy_u.py

--- internal energy_u.py
+++ internal energy_u.py
@@ -1,19 +1,20 @@
 from scipy.integrate import quad
-import math
 import numpy as np
 
 na = 6.022140857*10**23
-kb = 1.380649*10**(-23)
+kb = 1.38064852*10**(-23)
 
-def debye(x):
-    return (x**3)/(((np.exp(x)-1)))
+def u_vib(tD, tm):
+    result = [0]
+    temperature = [0]
+    debye = lambda x: x**3/(np.exp(x)-1)
+    
+    for t in np.arange(1, tm, 10):
+        temperature.append(t)
+        integrale_x = quad(debye, 0, tD/t)[0]
+        result.append(9*na*kb*t*(t/tD)**3*integrale_x)
+        
+    return temperature, result
 
-def u(t, thetaD):
-    return 9*na*kb*t*((t/thetaD)**3)*quad(debye, 0, thetaD/t)[0]
-
-vec_u = np.vectorize(u)
-
-def u_vib(thetaD, tm):
-    return(vec_u(np.arange(1.0, tm, 10), thetaD))
-
-print(u_vib(376, 933))
+if __name__ == "__main__": 
+    print (u_vib(376.447744459516, 933))
