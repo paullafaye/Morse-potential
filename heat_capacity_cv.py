f2e482849afc54d3465afb986d635929f7791226
paullafaye <62479777+paullafaye@users.noreply.github.com>
2020-04-17 17:16:34 -0400

    Update heat_capacity_cv.py

--- heat_capacity_cv.py
+++ heat_capacity_cv.py
@@ -1,19 +1,23 @@
 from scipy.integrate import quad
-import math
 import numpy as np
 
 na = 6.022140857*10**23
-kb = 1.380649*10**(-23)
+kb = 1.38064852*10**(-23)
 
-def debye(x):
-    return (x**4)*np.exp(x)/(((np.exp(x)-1)**2))
+def cv_vib(tD, tm):
+    result = [0]
+    temperature = [0]
+    debye = lambda x:(x**4)*np.exp(x)/(((np.exp(x)-1)**2))
+    
+    for t in np.arange(1, tm, 10):
+        temperature.append(t)
+        integral_x = quad(debye, 0, tD/t)[0]
+        result.append(9*na*kb*((t/tD)**3)*integral_x)
+        
+    return temperature, result
 
-def cv(t, thetaD):
-    return 9*na*kb*((t/thetaD)**3)*quad(debye, 0, thetaD/t)[0]
+if __name__ == "__main__": 
+    print (cv_vib(376, 933))
 
-vec_cv = np.vectorize(cv)
 
-def cv_vib(thetaD, tm):
-    return(vec_cv(np.arange(1.0, tm, 10), thetaD))
 
-print(cv_vib(376, 933))
